59c3b8188c87ab544be780906debf0b2969ebcb7
geronimo <geronimo@geronimo-Lenovo-G580.(none)>
2013-03-21 14:56:02 -0300

    init with home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlatTest::Application.config.secret_token = 'e13e50c649bdff0dceffd0030d76a4c2d452502fe24b55861b41b05ed7c9625b5d3aa68969d26df5c4e260ce8ed054d2f1eb4fac8f7829157a49355d47196bad'
